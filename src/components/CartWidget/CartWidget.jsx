780f2efe8a54e02025743f43f6f83beb2451c6dc
benjamin <benjaminaladio@gmail.com>
2021-11-08 15:54:25 -0300

    Widget e ItemListContainer

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BsFillCartCheckFill} from 'react-icons/bs';
+
+
+export const CartWidget = () => {
+    return (
+        <>
+        <a href="#">
+            <BsFillCartCheckFill style={{color:"#fff" , fontSize:40 , marginRight :20}} />
+        </a>
+        </>
+    )
+}
